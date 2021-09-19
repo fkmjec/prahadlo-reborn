fd75f6e2ec8bbcdfd646350852991414d72df670
František Kmječ <frantisek.kmjec@gmail.com>
2021-09-19 15:45:20 +0200

    Make the text interface a bit more robust

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,32 @@ mod network;
 mod text_interface;
 
 use network::*;
+use text_interface::*;
 
 fn main() {
     println!("Hello, world! Prahadlo here!");
     let nw = Network::new(Path::new("data/"));
     nw.print_debug_info();
-    while true {
-        let command = text_interface::get_command();
-        println!("{:?}", command);
+    loop {
+        let cmd = get_command();
+        match cmd {
+            Command::PrintNode(id) => {
+                let node = nw.get_node(id);
+                node.print_description();
+            },
+            Command::PrintStop(id) => {
+                match nw.get_stop(id) {
+                    Some(stop) => println!("{:?}", stop),
+                    None => println!("ERROR: no stop with such id")
+                }
+            },
+            Command::PrintTrip(id) => {
+                match nw.get_trip(id) {
+                    Some(trip) => println!("{:?}", trip),
+                    None => println!("ERROR: no trip with such id")
+                }
+            },
+            _ => (), 
+        }    
     }
 }
--- src/text_interface.rs
+++ src/text_interface.rs
@@ -1,9 +1,40 @@
 #[derive(Debug, PartialEq, Eq)]
 pub enum Command {
     Help,
+    Invalid,
     PrintNode(usize),
     PrintStop(String),
     PrintTrip(String),
+}
+
+fn parse_print_node(args: &[&str]) -> Command {
+    if args.len() == 1 {
+        let node_id = args[0].parse::<usize>();
+        match node_id {
+            Ok(id) => Command::PrintNode(id),
+            Err(_) => Command::Invalid,
+        }
+    } else {
+        Command::Invalid
+    }
+}
+
+fn parse_print_stop(args: &[&str]) -> Command {
+    if args.len() == 1 {
+        let stop_id = args[0];
+        Command::PrintStop(String::from(stop_id))
+    } else {
+        Command::Invalid
+    }
+}
+
+fn parse_print_trip(args: &[&str]) -> Command {
+    if args.len() == 1 {
+        let trip_id = args[0];
+        Command::PrintTrip(String::from(trip_id))
+    } else {
+        Command::Invalid
+    }
 }
 
 fn command_from_line(line: &str) -> Command {
@@ -11,30 +42,39 @@ fn command_from_line(line: &str) -> Command {
 
     // TODO: add validation of command arguments
     match args[0] {
-        "node" => Command::PrintNode(args[1].parse::<usize>().unwrap()),
-        "stop" => Command::PrintStop(String::from(args[1])),
-        "trip" => Command::PrintTrip(String::from(args[1])),
-        _ => Command::Help,
+        "node" => parse_print_node(&args[1..]),
+        "stop" => parse_print_stop(&args[1..]),
+        "trip" => parse_print_trip(&args[1..]),
+        "help" => Command::Help,
+        _ => Command::Invalid,
     }
 }
 
 fn print_help() {
     println!("Commands:");
-    println!("node [node_id] - prints information about a node with the id");
-    println!("stop [stop_id] - prints information about a stop with the id");
-    println!("trip [trip_id] - prints information about a trip with the id");
+    println!(" - node [node_id] - prints information about a node with the id");
+    println!(" - stop [stop_id] - prints information about a stop with the id");
+    println!(" - trip [trip_id] - prints information about a trip with the id");
+}
+
+fn print_invalid() {
+    
 }
 
 pub fn get_command() -> Command {
     let mut line = String::new();
-    println!("Enter the command :");
-    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("Enter the command: ");
+    std::io::stdin().read_line(&mut line).unwrap();
     let mut command = command_from_line(&line);
-    while command == Command::Help {
-        print_help();
+    while command == Command::Invalid || command == Command::Help {
+        if command == Command::Invalid {
+            println!("The command you entered was incorrect!");
+        } else {
+            print_help();
+        }
         let mut line = String::new();
         println!("Enter the command :");
-        let b1 = std::io::stdin().read_line(&mut line).unwrap();    
+        std::io::stdin().read_line(&mut line).unwrap();    
         command = command_from_line(&line);
     }
     command
